e6072929d773798f615decfef03cde6fa6b996c8
Jorgeonidas <zangetsujm@gmail.com>
2020-11-12 17:33:28 -0500

    layout class module, react scripts to 4.0.0

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';//wrapper auxiliar
+import classes from './Layout.module.css';
 
 //este va a ser el componente que encierra todo el cuerpo del app (wrapper)
 //usamos un high ordr component para tener varios elementos jsx sin requerir un padre
 //caso que queremos tener un elemento wrapper
-const lauyout = (props) =>(
+const lauyout = (props) => (
     <Aux>
         <div>
             Toolbar, Sidebar, Backdrop
-    </div>
-        <main>
+        </div>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Aux from '../../hoc/Auxiliary';
+
+class BurgerBulder extends Component{
+    render(){
+        return(
+            <Aux>
+                <div>
+                    Burger
+                </div>
+                <div>Build Controlls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBulder;
