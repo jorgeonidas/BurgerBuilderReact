648b691a079956adfb16a331092603b557308334
Jorgeonidas <zangetsujm@gmail.com>
2020-11-15 17:40:13 -0500

    build controlls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,15 +3,23 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    //obtener un array de keys de un objeto
-    const transformedIngredients = Object.keys(props.ingredients)//obtenemos el arreglo de keys del objeto
+    //necesitamos transformar el objeto que guarda los ingredientes en un array de ingredientes
+    let transformedIngredients = Object.keys(props.ingredients)//obtener un array de keys de un objeto
                                     .map(igkey => { //iteramos por cada key
-                                        return [...Array(props.ingredients[igkey])].map((_, i)=>{ //creamos un array del tamaño del valor del ingrediente y renderizamos por la cantidad de ingredientes
+                                        return [...Array(props.ingredients[igkey])].map((_, i)=>{ //por cada ingrediente mapeamos y creamos un array del tamaño de la cantidad de ingredientes para renderizar cada ingrediente
                                             return <BurgerIngredient key={igkey+i} type={igkey}/>
                                         }); 
-                                    });
+                                    })
+                                    .reduce((arr, el)=>{ //de un array de arrays a un array de objetos
+                                        return arr.concat(el);
+                                    }, []);
 
-            
+    console.log(transformedIngredients);
+
+    if(transformedIngredients.length === 0){
+        transformedIngredients = <p> Please start adding ingredients! </p>
+    }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 class BurgerBulder extends Component{
 
     // constructor(props){
@@ -10,10 +11,10 @@ class BurgerBulder extends Component{
 
     state ={
         ingredients :{
-            salad: 1,
-            bacon: 1,
-            cheese: 1,
-            meat: 1,
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0,
         }
     };
 
@@ -21,7 +22,7 @@ class BurgerBulder extends Component{
         return(
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Build Controlls</div>
+                <BuildControls/>
             </Aux>
         );
     }
