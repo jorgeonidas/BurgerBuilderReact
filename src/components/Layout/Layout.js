2a32b8d6bd04ed904ac718ebfcda812bc6f78dc3
Jorgeonidas <zangetsujm@gmail.com>
2020-11-21 10:37:29 -0500

    toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';//wrapper auxiliar
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 //este va a ser el componente que encierra todo el cuerpo del app (wrapper)
 //usamos un high ordr component para tener varios elementos jsx sin requerir un padre
 //caso que queremos tener un elemento wrapper
 const lauyout = (props) => (
     <Aux>
-        <div>
-            Toolbar, Sidebar, Backdrop
-        </div>
+        <Toolbar/>
         <main className={classes.Content}>
             {props.children}
         </main>
