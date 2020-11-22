b74a84b8ec32aa9466f8b7f328024528a5fb1300
Jorgeonidas <zangetsujm@gmail.com>
2020-11-22 13:16:56 -0500

    side drawer, side drawer toggle, logo mobile style, navigation items and item mobile style

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';//wrapper auxiliar
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 //este va a ser el componente que encierra todo el cuerpo del app (wrapper)
 //usamos un high ordr component para tener varios elementos jsx sin requerir un padre
 //caso que queremos tener un elemento wrapper
-const lauyout = (props) => (
-    <Aux>
-        <Toolbar/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Lauyout extends Component {
 
-export default lauyout;
+    state={
+        showSideDrawer: false
+    }
+
+    sideDrawerCloseHandler=()=>{
+        this.setState({showSideDrawer: false})
+    }
+
+    sideDrawerToggleHandler = () => {
+        //setear un estado que depende de un estado anterior
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer }
+        });
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>);
+    }
+};
+
+export default Lauyout;
